6d397f4773f017960739c1b6e551c8b20c72fc00
raysloks <raysloks@gmail.com>
2019-06-02 20:34:10 +0200

    more mobile work

--- RPG/RPG/Ability.h
+++ RPG/RPG/Ability.h
@@ -6,10 +6,10 @@ public:
 	Ability();
 	~Ability();
   
-  void activate(const AbilityContext& ac);
+  void activate(const AbilityContext& ac) const;
 
   float cast_time;
   Interruptability interruptability;
   
-  std::vector<std::shared_ptr<Effect>> effects;
+  std::vector<std::shared_ptr<const Effect>> effects;
 };
--- RPG/RPG/Effect.h
+++ RPG/RPG/Effect.h
@@ -3,5 +3,5 @@ class Effect
 public:
 	abstract ~Effect() = 0;
 	
-	abstract void apply(const AbilityContext& ac) = 0;
+	abstract void apply(const AbilityContext& ac) const = 0;
 };
